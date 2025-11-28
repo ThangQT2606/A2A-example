e42a727f0c1e8ee2928a23145bb5cbcc8aa90dde
ThangDQ <quangthang260602@gmail.com>
2025-11-28 10:59:15 +0700

    Add initial implementation of Weather Agent and Weather Executor, including web search functionality for weather information, asynchronous response streaming, and integration with the A2A framework for task management.

---
+++ src/langgraphAuth/weather_executor.py
@@ -0,0 +1,113 @@
+import logging
+from typing_extensions import override
+
+from a2a.server.agent_execution import AgentExecutor, RequestContext
+from a2a.server.events import EventQueue
+from a2a.server.tasks import TaskUpdater
+from a2a.types import (
+    InternalError,
+    InvalidParamsError,
+    Part,
+    TaskState,
+    TextPart,
+    UnsupportedOperationError,
+)
+from a2a.utils import (
+    new_agent_text_message,
+    new_task,
+)
+from a2a.utils.errors import ServerError
+
+from weather_agent import WeatherAgent
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+class WeatherExecutor(AgentExecutor):
+    """An AgentExecutor that runs an ADK-based Agent for weather."""
+    SUPPORTED_CONTENT_TYPES = ['text', 'text/plain']
+    def __init__(self, agent: WeatherAgent):
+        """Initializes the WeatherExecutor."""
+        logger.info(
+            f'Initializing WeatherExecutor'
+        )
+        if agent is None:
+            raise ValueError('Agent is required')
+        self.agent = agent
+        
+    @override
+    async def invoke(self, context: RequestContext, event_queue: EventQueue) -> None:
+        query = context.get_user_input()
+        task = context.current_task
+        if not task:
+            task = new_task(context.message)  # type: ignore
+            await event_queue.enqueue_event(task)
+        updater = TaskUpdater(event_queue, task.id, task.context_id)
+        response = await self.agent.invoke(query, task.context_id)
+        await updater.update_status(
+            TaskState.completed,
+            new_agent_text_message(response['content'], task.context_id, task.id),
+        )
+        await updater.complete()
+
+    @override
+    async def execute(
+        self,
+        context: RequestContext,
+        event_queue: EventQueue,
+    ) -> None:
+        error = self._validate_request(context)
+        if error:
+            raise ServerError(error=InvalidParamsError())
+
+        query = context.get_user_input()
+        task = context.current_task
+        if not task:
+            task = new_task(context.message)  # type: ignore
+            await event_queue.enqueue_event(task)
+        updater = TaskUpdater(event_queue, task.id, task.context_id)
+        try:
+            async for item in self.agent.stream(query, task.context_id):
+                is_task_complete = item['is_task_complete']
+                require_user_input = item['require_user_input']
+
+                if not is_task_complete and not require_user_input:
+                    await updater.update_status(
+                        TaskState.working,
+                        new_agent_text_message(
+                            item['content'],
+                            task.context_id,
+                            task.id,
+                        ),
+                    )
+                elif require_user_input:
+                    await updater.update_status(
+                        TaskState.input_required,
+                        new_agent_text_message(
+                            item['content'],
+                            task.context_id,
+                            task.id,
+                        ),
+                        final=True,
+                    )
+                    break
+                else:
+                    await updater.add_artifact(
+                        [Part(root=TextPart(text=item['content']))],
+                        name='conversion_result',
+                    )
+                    await updater.complete()
+                    break
+
+        except Exception as e:
+            logger.error(f'An error occurred while streaming the response: {e}')
+            raise ServerError(error=InternalError()) from e
+
+    def _validate_request(self, context: RequestContext) -> bool:
+        return False
+
+    @override
+    async def cancel(
+        self, context: RequestContext, event_queue: EventQueue
+    ) -> None:
+        raise ServerError(error=UnsupportedOperationError())
---
+++ src/weatherAgent/weather_agent.py
@@ -0,0 +1,140 @@
+import os
+from dotenv import load_dotenv
+from typing import Any, Literal
+from pydantic import BaseModel, Field
+from collections.abc import AsyncIterable
+
+from langchain.tools import tool, Tool
+from langchain_core.messages import SystemMessage, ToolMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.language_models import BaseChatModel
+from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
+
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import InMemorySaver
+
+load_dotenv()
+TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+search = TavilySearchAPIWrapper(tavily_api_key=TAVILY_API_KEY)
+
+@tool
+def search_web(query: str) -> str:
+    """Search weather information from the web"""
+    return search.results(query)
+
+class ResponseFormat(BaseModel):
+    """Respond to the user in this format."""
+
+    status: Literal['input_required', 'completed', 'error'] = 'input_required'
+    message: str
+    
+class WeatherAgent:
+    """WeatherAgent - a specialized assistant for weather information."""
+
+    SYSTEM_INSTRUCTION = SystemMessage(
+        content="""You are a helpful assistant that can search the web for weather information.
+        "You are a helpful assistant that can search the web for weather information."
+        You can use the following tools to get weather information:
+        search_web: Search the web for weather information
+        """
+    )
+
+    FORMAT_INSTRUCTION = (
+        'Set response status to input_required if you need more information from the user. '
+        'Set response status to error if something went wrong. '
+        'Set response status to completed if the request was fully answered.'
+    )
+
+    def __init__(self, llm: BaseChatModel):
+        """Initialize WeatherAgent with chosen LLM and weather search tool."""
+        self.tools = [search_web]
+
+        self.graph = create_react_agent(
+            model=llm,
+            tools=self.tools,
+            checkpointer=InMemorySaver(),
+            prompt=self.SYSTEM_INSTRUCTION,
+            response_format=(self.FORMAT_INSTRUCTION, ResponseFormat),
+        )
+        
+    def invoke(self, query: str, context_id: str) -> str:
+        """Invoke the weather agent with a query and context id."""
+        inputs = {'messages': [('user', query)]}
+        config = {'configurable': {'thread_id': context_id}}
+        self.graph.invoke(inputs, config)        
+        return self.get_agent_response(config)
+
+    async def stream(self, query: str, context_id: str) -> AsyncIterable[dict[str, Any]]:
+        """Stream intermediate and final responses from the weather agent."""
+        inputs = {'messages': [('user', query)]}
+        config = {'configurable': {'thread_id': context_id}}
+
+        for item in self.graph.stream(inputs, config, stream_mode='values'):
+            message = item['messages'][-1]
+
+            if isinstance(message, AIMessage) and message.tool_calls:
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Fetching the latest weather information...',
+                }
+
+            elif isinstance(message, ToolMessage):
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Processing weather data...',
+                }
+
+        yield self.get_agent_response(config)
+
+    def get_agent_response(self, config: dict[str, Any]) -> dict[str, Any]:
+        """Retrieve the structured final response from the LangGraph agent."""
+        current_state = self.graph.get_state(config)
+        structured_response = current_state.values.get('structured_response')
+
+        if structured_response and isinstance(structured_response, ResponseFormat):
+            status = structured_response.status
+            message = structured_response.message
+
+            if status == 'input_required':
+                return {
+                    'is_task_complete': False,
+                    'require_user_input': True,
+                    'content': message,
+                }
+
+            if status == 'error':
+                return {
+                    'is_task_complete': False,
+                    'require_user_input': True,
+                    'content': f'Error occurred: {message}',
+                }
+
+            if status == 'completed':
+                return {
+                    'is_task_complete': True,
+                    'require_user_input': False,
+                    'content': message,
+                }
+
+        return {
+            'is_task_complete': False,
+            'require_user_input': True,
+            'content': 'Unable to process your request at the moment. Please try again.',
+        }
+
+
+if __name__ == "__main__":
+    import asyncio
+    llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", 
+                             google_api_key=GOOGLE_API_KEY, 
+                             temperature=0.5, 
+                             top_p=1)
+    agent = WeatherAgent(llm=llm)
+    async def main():
+        res = agent.stream(query="What is the weather in Tokyo?", context_id="123")
+        async for item in res:
+            print(item)
+    asyncio.run(main())
